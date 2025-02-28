5db0f7574aad7383eddece6523c8229f2f704737
Victor Manuel Freixas Lemus <victormanuelfreixaslemus@vcv064192.vpn.uci.edu>
2025-02-28 09:21:42 -0800

    Detailed comments added for "calc_AO_overlap.py".

--- calc_AO_overlap.py
+++ calc_AO_overlap.py
@@ -1,4 +1,5 @@
-# This subroutine reads two xyz files and calculates the atomic overlap
+#   This subroutine reads two xyz files and returns the atomic orbital
+# overlaps for a given basis
 
 from ase import io
 from pyscf import gto
@@ -15,15 +16,19 @@ def read_xyz(xyzFile):
 	return atoms
 
 def calc_AO_overlap(xyzFile1, xyzFile2, basis):
-	#Reading the first xyz file:
+	#Reading xyz files:
 	mol1 = read_xyz(xyzFile1)
 	mol2 = read_xyz(xyzFile2)
+	#Concatenating molecules:
 	mol12 = mol1 + mol2
+	#Building Pyscf molecule objects
 	mol12_pyscf = gto.Mole()
 	mol12_pyscf.atom = mol12
 	mol12_pyscf.basis = basis
 	mol12_pyscf.build()
+	#Calculating atomic orbital overlaps
 	ao = mol12_pyscf.intor('int1e_ovlp')
+	#Returning the non-diagonal block of the overlap matrix
 	nOrb = int(mol12_pyscf.nao / 2)
 	return ao[:nOrb, nOrb:]
 
